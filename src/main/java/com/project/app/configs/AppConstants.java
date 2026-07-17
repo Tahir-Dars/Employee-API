a6e546dd117bfed7e37ca2140c9a5518b6a833fb
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-17 13:00:29 +0500

    Updated AppConstants to use String types for pagination values and added sorting order constants

--- src/main/java/com/project/app/configs/AppConstants.java
+++ src/main/java/com/project/app/configs/AppConstants.java
@@ -1,7 +1,9 @@
 package com.project.app.configs;
 
 public class AppConstants {
-    public static final Integer PAGE_NUMBER=0;
-    public static final Integer PAGE_SIZE=10;
-    public static final String SORT_EMPLOYES_USING = "firstName";
+    public static final String PAGE_NUMBER="0";
+    public static final String PAGE_SIZE="10";
+    public static final String SORT_ORDER = "asc";
+    public static final String SORT_EMPLOYES_USING_F = "firstName";
+    public static final String SORT_EMPLOYES_USING_L = "firstName";
 }
