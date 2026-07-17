c677b6e5fdbea38de9ed04a92e1e7b6864c730e9
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-17 11:58:51 +0500

    Added ModelMapper dependency and configuration for object mapping

---
+++ src/main/java/com/project/app/configs/ApplicationConfigs.java
@@ -0,0 +1,15 @@
+package com.project.app.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ApplicationConfigs {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
