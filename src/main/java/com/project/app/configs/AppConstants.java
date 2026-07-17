5d8eeab15e41e8c18b2e5aebd5933c0b6c2185ae
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-17 15:54:48 +0500

    Updated AppConstants to include sorting by salary and corrected last name sorting constant

--- src/main/java/com/project/app/configs/AppConstants.java
+++ src/main/java/com/project/app/configs/AppConstants.java
@@ -5,5 +5,6 @@ public class AppConstants {
     public static final String PAGE_SIZE="10";
     public static final String SORT_ORDER = "asc";
     public static final String SORT_EMPLOYES_USING_F = "firstName";
-    public static final String SORT_EMPLOYES_USING_L = "firstName";
+    public static final String SORT_EMPLOYES_USING_Salary = "salary";
+    public static final String SORT_EMPLOYES_USING_L = "lastName";
 }
