41b91a926cd1c5efd63b974154288be98c4d2307
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-17 11:41:20 +0500

    Added API_ResponseDTO class and updated AppConstants to use Integer for page number and size

--- src/main/java/com/project/app/configs/AppConstants.java
+++ src/main/java/com/project/app/configs/AppConstants.java
@@ -1,6 +1,7 @@
 package com.project.app.configs;
 
 public class AppConstants {
-    public static final String PAGE_NUMBER="0";
-    public static final String PAGE_SIZE="10";
+    public static final Integer PAGE_NUMBER=0;
+    public static final Integer PAGE_SIZE=10;
+    public static final String SORT_EMPLOYES_USING = "firstName";
 }
